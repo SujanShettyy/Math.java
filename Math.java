e1e6f9db28b72b486a29cb8c48943fb533acc86b
Sujan Shetty <144237005+SujanShettyy@users.noreply.github.com>
2024-04-30 19:22:09 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,19 @@
+
+public class Math {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int min = a - b;
+        int div = a / b;
+        int mul = a * b;
+        int mod = a % b;
+        System.out.println("The addition of "+a+" and "+b+" is: "+sum);
+        System.out.println("The subtraction of "+a+" and "+b+" is: "+min);
+        System.out.println("The divition of "+a+" and "+b+" is: "+div);
+        System.out.println("The multiplication of "+a+" and "+b+" is: "+mul);
+        System.out.println("The modulation of "+a+" and "+b+" is: "+mod);
+
+    }
+    
+}
